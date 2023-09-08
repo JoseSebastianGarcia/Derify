7498b317b0301a554b7cc98703d7930f2879eeb8
Sebastián Garcia <josesgar@emeal.nttdata.com>
2023-09-08 10:12:19 -0300

    mejoras en generacion de enlaces

--- Derify.Core/wwwroot/js/script.js
+++ Derify.Core/wwwroot/js/script.js
@@ -13,135 +13,67 @@ $(document).ready(function(){
 
     function LoadLinks()
     {
+        $("#svg-container").empty();
         var links = $(".link");
         
-        var lineStyle = "2px solid #AA969696";
-        var noneStyle = "none";
-
         for(var i = 0; i < links.length; i++)
         {
             var link = links[i];
             var sourceId = link.getAttribute("Source");
             var targetId = link.getAttribute("Target");
 
-            if(sourceId != undefined && targetId != undefined && sourceId != null && targetId != null)
-            {
+            if (sourceId != undefined && targetId != undefined && sourceId != null && targetId != null) {
                 var sourceEl = document.getElementById(sourceId);
                 var targetEl = document.getElementById(targetId);
 
-                var sourceRect = sourceEl.getBoundingClientRect();
-                var targetRect = targetEl.getBoundingClientRect();
-                
-                const x1 = sourceRect.left + sourceRect.width / 2;
-                const y1 = sourceRect.top + sourceRect.height / 2;
-                
-                const x2 = targetRect.left + targetRect.width / 2;
-                const y2 = targetRect.top + targetRect.height / 2;
-                
-                var handled = false;
-                
-                // Determina la posición relativa de Entity1 en relación a Entity2
-                if (!handled && x1 < x2 && y1 < y2) {
-                    // Entity1 está en el cuadrante superior izquierdo de Entity2
-                    // Casuística 1
-                    $(link).css({
-                        left: Math.min(x1, x2),
-                        top: Math.min(y1, y2),
-                        width: Math.abs(x2 - x1),
-                        height: Math.abs(y2 - y1),
-                        'border-bottom': lineStyle,   // Línea inferior
-                        'border-right': noneStyle,     // Línea derecha
-                        'border-top':noneStyle,
-                        'border-left':lineStyle
-                    });
-                    handled = true;
-                } else if (!handled && x1 === x2 && y1 < y2) {
-                    // Entity1 está en la misma línea vertical arriba de Entity2
-                    // Casuística 2
-                    $(link).css({
-                        left: Math.min(x1, x2),
-                        top: Math.min(y1, y2),
-                        width: Math.abs(x2 - x1),
-                        height: Math.abs(y2 - y1),
-                        'border-top': lineStyle   // Línea superior
-                    });
-                    handled = true;
-                } else if (!handled && x1 > x2 && y1 < y2) {
-                    // Entity1 está en el cuadrante superior derecho de Entity2
-                    // Casuística 3
-                    $(link).css({
-                        left: Math.min(x1, x2),
-                        top: Math.min(y1, y2),
-                        width: Math.abs(x2 - x1),
-                        height: Math.abs(y2 - y1),
-                        'border-bottom': lineStyle,   // Línea inferior
-                        'border-right': lineStyle,     // Línea derecha
-                        'border-top':noneStyle,
-                        'border-left':noneStyle
-                    });
-                    handled = true;
-                } else if (!handled && x1 < x2 && y1 === y2) {
-                    // Entity1 está en la misma línea horizontal a la izquierda de Entity2
-                    // Casuística 4
-                    $(link).css({
-                        left: Math.min(x1, x2),
-                        top: Math.min(y1, y2),
-                        width: Math.abs(x2 - x1),
-                        height: Math.abs(y2 - y1),
-                        'border-left': lineStyle   // Línea izquierda
-                    });
-                    handled = true;
-                } else if (!handled && x1 > x2 && y1 === y2) {
-                    // Entity1 está en la misma línea horizontal a la derecha de Entity2
-                    // Casuística 6
-                    $(link).css({
-                        left: Math.min(x1, x2),
-                        top: Math.min(y1, y2),
-                        width: Math.abs(x2 - x1),
-                        height: Math.abs(y2 - y1),
-                        'border-right': lineStyle  // Línea derecha
-                    });
-                    handled = true;
-                } else if (!handled && x1 < x2 && y1 > y2) {
-                    // Entity1 está en el cuadrante inferior izquierdo de Entity2
-                    // Casuística 7
-                    $(link).css({
-                        left: Math.min(x1, x2),
-                        top: Math.min(y1, y2),
-                        width: Math.abs(x2 - x1),
-                        height: Math.abs(y2 - y1),
-                        'border-bottom': lineStyle,   // Línea inferior
-                        'border-right': lineStyle,     // Línea derecha
-                        'border-top':noneStyle,
-                        'border-left':noneStyle,
-                    });
-                    handled = true;
-                } else if (!handled && x1 === x2 && y1 > y2) {
-                    // Entity1 está en la misma línea vertical debajo de Entity2
-                    // Casuística 8
-                    $(link).css({
-                        left: Math.min(x1, x2),
-                        top: Math.min(y1, y2),
-                        width: Math.abs(x2 - x1),
-                        height: Math.abs(y2 - y1),
-                        'border-bottom': lineStyle   // Línea inferior
-                    });
-                    handled = true;
-                } else if (!handled && x1 > x2 && y1 > y2) {
-                    // Entity1 está en el cuadrante inferior derecho de Entity2
-                    // Casuística 9
-                    $(link).css({
-                        left: Math.min(x1, x2),
-                        top: Math.min(y1, y2),
-                        width: Math.abs(x2 - x1),
-                        height: Math.abs(y2 - y1),
-                        'border-bottom': lineStyle,   // Línea inferior
-                        'border-right': noneStyle,     // Línea derecha
-                        'border-top':noneStyle,
-                        'border-left':lineStyle,
-                    });
-                    handled = true;
-                }
+                var sourceRect = window.getComputedStyle(sourceEl);
+                var targetRect = window.getComputedStyle(targetEl);
+
+                var rects = {
+                    source: {
+                        left: parseFloat(sourceRect.getPropertyValue('left')),
+                        top: parseFloat(sourceRect.getPropertyValue('top')),
+                        width: parseFloat(sourceRect.getPropertyValue('width')),
+                        height: parseFloat(sourceRect.getPropertyValue('height'))
+                    },
+                    target: {
+                        left: parseFloat(targetRect.getPropertyValue('left')),
+                        top: parseFloat(targetRect.getPropertyValue('top')),
+                        width: parseFloat(targetRect.getPropertyValue('width')),
+                        height: parseFloat(targetRect.getPropertyValue('height'))
+                    }
+                };
+
+                const x1 = rects.source.left + rects.source.width / 2;
+                const y1 = rects.source.top + rects.source.height / 2;
+
+                const x2 = rects.target.left + rects.target.width / 2;
+                const y2 = rects.target.top + rects.target.height / 2;
+
+                // Crea un elemento SVG para la línea de conexión
+                var svgns = "http://www.w3.org/2000/svg";
+                var svg = document.createElementNS(svgns, "svg");
+                var w = (x1 > x2) ? x1 : x2;
+                var h = (y1 > y2) ? y1 : y2;
+
+                svg.setAttribute("width", w + "px");
+                svg.setAttribute("height", h + "px");
+                svg.setAttribute("style", "margin: 0px; padding: 0px; z-index:-1; position:absolute; left:0px; top:0px;");
+                svg.setAttribute("xmlns", svgns);
+                svg.setAttribute("version", "1.1");
+
+                var line = document.createElementNS(svgns, "line");
+                line.setAttribute("x1", x1);
+                line.setAttribute("y1", y1);
+                line.setAttribute("x2", x2);
+                line.setAttribute("y2", y2);
+                line.setAttribute("stroke", "#AA969696");
+                line.setAttribute("stroke-width", "2");
+
+                svg.appendChild(line);
+
+                // Agrega el SVG al contenedor
+                $("#svg-container").append(svg);
             }
         }
     }
